9c0b2ff0bfd8a431b64607e90c89d6b25934f577
Jarkapong Palawong <beegalnwza007@gmail.com>
2026-03-06 17:00:48 +0700

    Bug fix#2 – adding student id validation (id must be composed of numbers between 0 and 9

--- validate.js
+++ validate.js
@@ -16,19 +16,30 @@ function checkCandiNo() {
   }
 }
 
-function validateForm(){
-	if(!checkSID()){
-	  alert("Invalid value for Student ID!");
-	  document.getElementById("sid").focus();
-	  return false;
-	}else{
-		if(!checkCandiNo()){
-		  alert("Invalid value for Candidate No!!");
-		  document.getElementById("candi").focus();
-		  return false;
-		}else{
-			alert("Your input data passes validation!!");
-			return true;
-		}
-	}
+function validateForm() {
+ 
+    let sid = document.getElementById("sid").value.trim();
+ 
+    
+    if (sid == "") {
+        alert("Please enter Student ID");
+        return false;
+    }
+ 
+    
+    if (sid.length != 10) {
+        alert("Student ID must be 10 characters");
+        return false;
+    }
+ 
+    
+    let pattern = /^[0-9]+$/;
+ 
+    if (!pattern.test(sid)) {
+        alert("Student ID must contain only numbers (0-9)");
+        return false;
+    }
+ 
+    return true;
 }
+	
